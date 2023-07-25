93fdbade8d156595cdf9d28b74af63b317414838
sudeeshbhaskar <sudhisubi73@gmail.com>
2023-07-25 11:07:39 +0530

    Apollo server setup

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import {ApolloServer} from '@apollo/server'
+import {startStandaloneServer} from '@apollo/server/standalone'
+import {gql } from 'graphql-tag'
+
+
+const typeDefs = gql`
+type Query{
+    hello: String!
+}
+`
+
+const resolvers={
+    Query:{
+        hello:()=>"dd"
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+  
+
+  const startServer = async()=>{
+
+    const { url } = await startStandaloneServer(server, {
+        listen: { port: 4000 },
+      });
+      console.log(`🚀  Server ready at: ${url}`);
+  }
+
+  startServer();
